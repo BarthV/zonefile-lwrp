66f938322f02bee2c2ef873557f1e8b9fbebb77e
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2013-12-27 13:18:27 +0100

    first commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'zonefile-lwrp'
+maintainer       'Barthélemy Vessemont'
+maintainer_email ''
+license          'GNU GPL v3'
+description      'Some Chef LWRP to manage and edit bind zonefiles'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
