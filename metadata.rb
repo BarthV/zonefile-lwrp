b0bcaa51842341d9b90969d4bde2f307bccf85ab
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2013-12-29 22:38:13 +0100

    tag

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name             'zonefile'
 maintainer       'Barthélemy Vessemont'
 maintainer_email ''
 license          'GNU GPL v3'
-description      'Some Chef LWRP to manage and edit bind zonefiles'
+description      'Some Chef LWRP to manage and edit SOA from bind zonefiles'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.9.0'
 
