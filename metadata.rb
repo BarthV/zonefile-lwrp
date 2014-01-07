9c221d95f75fcc9d33ec305dd093d0c3734a2edb
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2014-01-07 11:21:26 +0100

    messing with versions :/

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email ''
 license          'GNU GPL v3'
 description      'Some Chef LWRP to manage and edit SOA from bind zonefiles'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.9.2'
+version          '0.9.1'
 
