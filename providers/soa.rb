e58c4ed792176c3bfcd7d303f2d6eb65de23c414
Barthelemy Vessemont <barthelemy.vessemont@photobox.com>
2014-01-07 10:27:50 +0100

    debug

--- providers/soa.rb
+++ providers/soa.rb
@@ -71,6 +71,24 @@ def load_current_resource
   @current_resource.globalttl == globalttl_from_file
     @current_resource.update_needed = false
   end
+
+  puts "
+
+==== DEBUG ====
+nameserver = #{@current_resource.nameserver == soa_from_file(:primary)}
+contact = #{@current_resource.contact == soa_from_file(:email)}
+soattl = #{@current_resource.soattl == soa_from_file(:ttl)}
+refresh = #{@current_resource.refresh == soa_from_file(:refresh)}
+retrydelay = #{@current_resource.retrydelay == soa_from_file(:retry)}
+expire = #{@current_resource.expire == soa_from_file(:expire)}
+neg_cache_ttl = #{@current_resource.neg_cache_ttl == soa_from_file(:minimumTTL)}
+origin = #{@current_resource.origin == origin_from_file}
+globalttl = #{@current_resource.globalttl == globalttl_from_file}
+
+update_needed ? = #{@current_resource.update_needed}
+
+"
+
 end
 
 
